8bfc0641b7594d5f863e6495dc5dd1dcbbf60cd6
Ubuntu <ubuntu@ip-172-31-33-231.ec2.internal>
2020-11-18 22:23:24 +0000

    initial commit

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Jonathan Pasco-Arnone all rights reserved
+//
+// Created by Jonathan Pasco-Arnone
+// Created on Novemeber 2020
+// This program outputs "Hello, World!"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
